71da8d153c140b9d3ad706ca5d49db0ce82f8f1f
Enyioma Nwadibia <fynewily@gmail.com>
2023-10-31 22:31:13 +0100

    env var encryption

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -84,8 +84,6 @@ resource "aws_iam_role_policy_attachment" "hostspace-AmazonEKSClusterPolicy" {
   role       = aws_iam_role.hostspace.name
 }
 
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "hostspace-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.hostspace.name
