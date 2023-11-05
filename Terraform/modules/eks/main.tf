8b620dde0c0a97ff8aec335d6fb7109b9fab83bf
Enyioma Nwadibia <fynewily@gmail.com>
2023-11-05 21:38:19 +0100

    iac config

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -58,6 +58,14 @@ resource "aws_security_group" "node_group_one" {
 
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port = 5000
+    to_port   = 5000
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
